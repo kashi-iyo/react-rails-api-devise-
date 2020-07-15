655a02b0e94f116b148d682f3ad29f5ccea81bed
kasi-iyo <snakeman143@gmail.com>
2020-07-15 12:33:07 +0900

    turbolinkをimport /サンプルのReactコンポーネント削除

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -6,7 +6,8 @@
 //
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
-
+import Turbolinks from 'turbolinks'
+Turbolinks.start()
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -15,8 +16,6 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
-
-require('./hello_react')
+require("./components/TodoApp");
 require('bootstrap')
 import 'bootstrap/dist/css/bootstrap'
