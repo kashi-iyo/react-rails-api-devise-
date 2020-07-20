e33b9278086bac75e3ed10daec344dc79ced0bb6
kasi-iyo <snakeman143@gmail.com>
2020-07-20 12:28:48 +0900

    capybaraに、JSドライバはChromeを使用すること／テスト実行時・実行後にDatabaseCleanerする設定

---
+++ spec/support/capybara.rb
@@ -0,0 +1,38 @@
+require 'capybara/rspec'
+
+Capybara.javascript_driver = :selenium_chrome
+
+RSpec.configure do |config|
+
+  config.use_transactional_fixtures = false
+
+  config.before(:suite) do
+    if config.use_transactional_fixtures?
+      raise(<<-MSG)
+
+      MSG
+    end
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, type: :feature) do
+    driver_shares_db_connection_with_specs = Capybara.current_driver == :rack_test
+
+    unless driver_shares_db_connection_with_specs
+      DatabaseCleaner.strategy = :truncation
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.append_after(:each) do
+    DatabaseCleaner.clean
+  end
+
+end
